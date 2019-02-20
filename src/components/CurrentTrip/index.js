dee5a0e50bf48b6d1ff9b3da8964afc00d38e16f
Tom Horton <thomasphorton@gmail.com>
2019-02-19 19:59:07 -0800

    Working on real-time maps.

--- src/components/CurrentTrip/index.js
+++ src/components/CurrentTrip/index.js
@@ -13,7 +13,7 @@ class Trip extends Component {
 
   render() {
     return (
-      <Connector mqttProps="ws://192.168.1.108:9001">
+      <Connector mqttProps="ws://localhost:1883">
         <div>
           <h2 className="post-title">{this.state.dateString} {this.state.timeString}</h2>
           <p className="post-meta">trip id: {this.state.tripId}</p>
--- src/components/MessageContainer/index.js
+++ src/components/MessageContainer/index.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import { subscribe } from 'mqtt-react';
+import Map from '../Map';
 
 class MessageList extends Component {
 
   render(props) {
-
+    // console.log(this.props.data);
     var data;
 
     if (this.props.data[0]) {
-      data = JSON.stringify(this.props.data[0].data);
+      data = JSON.stringify(this.props.data[0]);
+      console.log(data);
     }
 
     return (
       <div>
+        <Map  />
         <p>{ data }</p>
       </div>
     );
@@ -20,5 +23,5 @@ class MessageList extends Component {
 }
 
 export default subscribe({
-  topic: 'sensor-data'
+  topic: 'gps-data'
 })(MessageList);
